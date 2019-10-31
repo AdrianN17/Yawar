613ca72a6d40a78085c2942cf9b98c7584324a91
juancarlosmz <mendozazabaletajuancarlos@gmail.com>
2019-10-30 23:11:03 -0500

    0.12.3
    animacion ahogar, ataquelanza, imagenes de las papas y maiz

--- Assets/Script/NPC/sacerdote.cs
+++ Assets/Script/NPC/sacerdote.cs
@@ -9,10 +9,11 @@ public class sacerdote : MonoBehaviour
     public Text texto_npc;
     public List<string> lista_texto;
     public GameObject mensaje;
+    public GameObject objsacerdote;
     public int index;
     void Start()
     {
-        lista_texto = new List<string> { "Que es lo que tienes para mi? "};
+        lista_texto = new List<string> { "Que es lo que tienes para mi? - pulsa A para entregar objetos" };
 
     }
 
@@ -24,12 +25,17 @@ public class sacerdote : MonoBehaviour
 
     public void mostrar()
     {
+
+        var animacionSacerdote = objsacerdote.GetComponent<Animator>();
+        animacionSacerdote.SetBool("PedirObjetos", true);
         mensaje.SetActive(true);
         texto_npc.text = lista_texto[index];
     }
 
     public void nomostrar()
     {
+        var animacionSacerdote = objsacerdote.GetComponent<Animator>();
+        animacionSacerdote.SetBool("PedirObjetos", false);
         mensaje.SetActive(false);
         texto_npc.text = " ";
     }
