9a3de802bbef4ca46d9d97356e6d0530d12e240b
juancarlosmz <mendozazabaletajuancarlos@gmail.com>
2019-11-01 10:36:07 -0500

    0.13.1
    Prefab Pez mas animacion, sonidos - script en el pj para el audio. animacion recibir daño (personaje y enemigo)

---
+++ Assets/Script/Sonidos/Sonidos_Pj.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonidos_Pj : MonoBehaviour
+{
+    AudioSource aSource;
+    
+    public List<AudioClip> Audios;
+    void Start()
+    {
+        Audios.AddRange(Resources.LoadAll<AudioClip>("Sonidos") as AudioClip[]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
+        {
+            aSource.PlayOneShot(Audios[1]);
+        }
+    }
+}
---
+++ Assets/Script/Sonidos/fondo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fondo : MonoBehaviour
+{
+    AudioSource aSource;
+    public AudioClip SonidoFondo;
+    void Start()
+    {
+        aSource.PlayOneShot(SonidoFondo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
